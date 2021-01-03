f9ce49e8c42b1356a4aa4b32189432d1d7aa63da
Finn Pauls <mbp@pauls.dev>
2021-01-03 01:56:09 +0100

    Add a shuffle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {shuffle} from 'lodash'
 import styled from 'styled-components'
-import books from '../books.json'
+import allBooks from '../books.json'
+import {Book} from '../types'
 import BookThumbnail from './BookThumbnail'
 
 export default function App() {
+  const [books, setBooks] = useState<Book[]>(allBooks)
   return (
     <Main>
       <h2>📚 browse books 📚</h2>
+      <Button onClick={shuffleBooks}>Shuffle</Button>
       <BooksContainer>
         {books.map((book) => (
           <BookThumbnail key={book.image} {...book} />
@@ -14,6 +18,10 @@ export default function App() {
       </BooksContainer>
     </Main>
   )
+
+  function shuffleBooks() {
+    setBooks((books) => shuffle(books))
+  }
 }
 
 const Main = styled.div`
@@ -32,4 +40,28 @@ const BooksContainer = styled.div`
   flex-wrap: wrap;
   align-items: flex-start;
   justify-content: center;
+`
+
+const Button = styled.button`
+  font-size: 20px;
+  padding: 12px 40px;
+  border: 2px solid #242422;
+  background-color: transparent;
+  color: #242422;
+  margin-bottom: 20px;
+  cursor: pointer;
+  border-radius: 7px;
+  box-shadow: 0px 2px white;
+
+  transition: background-color 0.2s, color 0.2s;
+
+  :hover {
+    background-color: #242422;
+    color: white;
+  }
+
+  :active {
+    transform: translate(0, 2px);
+    box-shadow: 0px 0px white;
+  }
 `
