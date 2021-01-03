20dd2ec82c24d6f6815fcc8b6e613d6fbb64af16
Finn Pauls <mbp@pauls.dev>
2021-01-03 01:50:50 +0100

    Add a headline

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import BookThumbnail from './BookThumbnail'
 export default function App() {
   return (
     <Main>
+      <h2>📚 browse books 📚</h2>
       <BooksContainer>
         {books.map((book) => (
           <BookThumbnail key={book.image} {...book} />
@@ -19,10 +20,11 @@ const Main = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 
   background-color: #eeeee4;
   color: #242422;
-  font-size: 75px;
+  font-size: 30px;
 `
 
 const BooksContainer = styled.div`
