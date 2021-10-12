9d9dfdfc0479b34e20b5886a4e7666a93f4a607d
lexenson <alexander.assmann91@gmail.com>
2021-10-12 11:12:07 +0200

    Show title & genres when hovering
    Otherwise all people could do was check out the covers before leaving
    the website. We should consider also scraping short summaries of the
    books.

--- src/BookThumbnail.tsx
+++ src/BookThumbnail.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import {Book} from '../types'
+import {take} from 'lodash'
 
-export default function BookThumbnail({image, title, color, isbn10}: Book) {
+export default function BookThumbnail({
+  image,
+  title,
+  color,
+  isbn10,
+  genres
+}: Book) {
+  const [hovering, setHovering] = useState<boolean>(false)
+
   const src = image
     .replace('._SY75_', '')
     .replace('._SX50_', '')
@@ -19,8 +28,16 @@ export default function BookThumbnail({image, title, color, isbn10}: Book) {
       rel="noreferrer"
       href={href}
       backgroundColor={color}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
     >
+      <Title shouldShow={hovering}>{title}</Title>
       <Cover src={src} alt={title} />
+      <Info shouldShow={hovering}>
+        {take(genres, 3).map((genre, index) => (
+          <GenreTag key={index}>{genre}</GenreTag>
+        ))}
+      </Info>
     </LinkContainer>
   )
 }
@@ -48,6 +65,50 @@ const LinkContainer = styled.a<{
       z-index: 10;
     }
   }
+`
+const Title = styled.h2<{
+  shouldShow: boolean
+}>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 268;
+  margin: 0;
+  padding: 16px;
+  font-size: 20px;
+  display: ${({shouldShow}) => (shouldShow ? 'flex' : 'none')};
+  justify-content: space-between;
+  align-items: center;
+  background-color: #3e3e3b;
+  color: #eee;
+`
+
+const Info = styled.div<{
+  shouldShow: boolean
+}>`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 268;
+  margin: 0;
+  padding: 16px;
+  font-size: 16px;
+  display: ${({shouldShow}) => (shouldShow ? 'flex' : 'none')};
+  justify-content: center;
+  flex-direction: row-reverse;
+  flex-wrap: wrap;
+`
+
+const GenreTag = styled.div`
+  background-color: #444444;
+  color: #eee;
+  border-radius: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 8px;
+  margin-bottom: 8px;
+  padding: 4px 8px;
 `
 
 const Cover = styled.img.attrs({width: 300, loading: 'lazy'})`
