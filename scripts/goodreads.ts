e0c6a29435dbfda1fea43f35f229dde34dd95626
Finn Pauls <mbp@pauls.dev>
2021-10-14 22:32:31 +0200

    Scrape Shelves from GoodReads

--- scripts/goodreads.ts
+++ scripts/goodreads.ts
@@ -3,50 +3,82 @@ import got from 'got'
 import {JSDOM} from 'jsdom'
 import {Book, BookDetails} from '../types'
 import {showColors} from 'dominant-colors'
+import envmate from 'envmate'
+import {str} from 'envalid'
+
+const env = envmate({
+  GOODREADS_COOKIE: str()
+})
 
 async function main() {
-  console.log('Downloading best books..')
-
-  let books = []
-
-  for (let page = 1; page <= 5; page++) {
-    books.push(...(await getBooksFromPage(page)))
-  }
-
-  for (const book of books) {
-    const {isbn10, genres} = await getBookDetails(book.url)
-    book.isbn10 = isbn10
-    book.genres = genres
-    console.log(book.isbn10)
-  }
-
-  writeFileSync('books.json', JSON.stringify(books, null, '  '))
-  console.log('Done')
+  await downloadShelf('nonfiction')
+  await downloadShelf('fiction')
 }
 
 main()
 
-async function getBooksFromPage(page: number): Promise<Book[]> {
+async function downloadShelf(shelf: string) {
+  console.log(`Downloading best books in ${shelf}..`)
+
+  let books = []
+
+  for (let page = 1; page <= 5; page++) {
+    books.push(...(await getBooks(shelf, page)))
+  }
+
+  writeFileSync(
+    `./data/shelves/${shelf}.json`,
+    JSON.stringify(books, null, '  ')
+  )
+  console.log(`${shelf}.json created`)
+}
+
+async function getBooks(shelf: string, page: number): Promise<Book[]> {
   console.log(`On page ${page}...`)
   const {body} = await got(
-    `https://www.goodreads.com/list/show/1.Best_Books_Ever?page=${page}`
+    `https://www.goodreads.com/shelf/show/${shelf}?page=${page}`,
+    {
+      headers: {
+        cookie: env.GOODREADS_COOKIE
+      }
+    }
   )
   const document = new JSDOM(body).window.document
 
-  const bookElements = document.querySelectorAll('[itemtype*=Book]')
+  const bookElements = document.querySelectorAll('.leftContainer .elementList')
   return await Promise.all(
     Array.from(bookElements).map(async (bookElement) => {
-      const image = bookElement
-        .querySelector('[itemprop=image]')
-        .getAttribute('src')
+      try {
+        const image = bookElement.querySelector('a img').getAttribute('src')
 
-      const color: string = (await showColors(image, 1))[0]
+        const color: string = (await showColors(image, 1))[0]
 
-      const url = bookElement
-        .querySelector('[itemprop=url]')
-        .getAttribute('href')
-      const title = bookElement.querySelector('[itemprop=name]').innerHTML
-      return {image, url, title, color}
+        const url = bookElement.querySelector('a').getAttribute('href')
+        const title = bookElement.querySelector('a img').getAttribute('alt')
+
+        const miniRatingElement = bookElement.querySelector(
+          '.smallText:not(.authorName)'
+        )
+
+        const [
+          avgRatingString,
+          numRatingString,
+          publishedString
+        ] = miniRatingElement.textContent.trim().split('—')
+
+        const rating = parseFloat(
+          avgRatingString.replace('avg rating ', '').trim()
+        )
+        const numRatings = parseInt(numRatingString.trim().replace(/,/g, ''))
+        const published = parseInt(
+          publishedString.trim().replace('published ', '')
+        )
+
+        return {image, url, title, color, rating, numRatings, published}
+      } catch (e) {
+        console.log(e)
+        console.log(bookElement.innerHTML)
+      }
     })
   )
 }
