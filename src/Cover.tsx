f5629badd2bd2a6f3077248f7577fc204459fe2b
lexenson <alexander.assmann91@gmail.com>
2021-10-16 12:05:36 +0200

    Fix image aspect ratio
    In the book modal books would stretch as long as the modal would be, so
    for longer book descriptions the image was distorted.

--- src/Cover.tsx
+++ src/Cover.tsx
@@ -13,6 +13,8 @@ export default function Cover({book, ...props}: {book: Book}) {
 }
 
 const Image = styled.img.attrs({width: 300, loading: 'lazy'})`
+  object-fit: cover;
   width: 300px;
+  height: 100%;
   border-radius: 2px 8px 8px 2px;
 `
